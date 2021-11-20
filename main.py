100e1ea70262fb40085ac22502a91568c18b7d25
knock_nott <77430863+Nikhil-Jagtap619@users.noreply.github.com>
2021-11-20 23:08:33 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -3,27 +3,30 @@ import random
 correct = 0
 incorrect = 0
 game = True
-rand_num = random.randint(1,9)
+rand_num = random.randint(1,10)
+print("Welcome to Guessing Game")
 
 while game:
-    print("Welcome to the Guesing Game")
-    ans = input("Enter your number between 1 and 10 : ")
+    ans = input("Guess number between 1 and 10 : ")
     if ans.isnumeric():
         ans = int(ans)
         if ans == rand_num:
             correct+=1
-            print("Congrats","you guessed it right!")
+            print("you guessed it right!")
         elif ans < rand_num:
             incorrect+=1
-            print("Warning","incorrect! too low")
+            print("incorrect! too low")
         else:
-            print("Warning","incorrect! too high")
+            incorrect+=1
+            print("incorrect! too high")
     elif ans.isalpha():
-        if ans.lower() == "exit":
+        if ans.lower() == "quit":
             game = False
         else:
-            print("Warning","type 'exit' to quit the game \n or enter the number")
+            print("type 'quit' to exit the game \n or enter the number")
     else:
-        print("Warning","Invalid Input!")
+        print("Invalid Input!")
+
+
 print("Thank you for playing")
 print(f"your score \n correct: {correct} \n incorrect: {incorrect} \n total attempt: {correct+incorrect}")
